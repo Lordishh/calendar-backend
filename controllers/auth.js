711d47691ddaf908c936e343046fa97d52fb41e6
Enuel <enuel.monzon@gmail.com>
2022-07-06 20:37:19 -0300

    agregado express validator y custom middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,14 +5,8 @@ const crearUsuario = (req, res = response ) => {
 
   const { name, email, password} = req.body;
 
-  if ( name.length < 5 ) {
-    return res.status(400).json({
-      ok: false,
-      msg: 'El nombre debe tener más de 5 letras'
-    });
-  }
 
-  res.json({
+  res.status(201).json({
     ok: true,
     msg: 'registro',
     name,
@@ -24,6 +18,7 @@ const crearUsuario = (req, res = response ) => {
 const loginUsuario = (req, res = response ) => {
 
   const { email, password } = req.body;
+
 
   res.json({
     ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,33 @@
 */
 
 const { Router } = require('express');
-const router = Router();
-
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
-router.post('/register', crearUsuario );
+const router = Router();
 
-router.post('/', loginUsuario);
+
+
+
+router.post(
+  '/register',
+  [ // middlewares
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser mínimo de 6 caracteres').isLength({ min: 6 }),
+    validarCampos
+  ],
+  crearUsuario );
+
+router.post(
+  '/',
+  [ // middlewares
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser mínimo de 6 caracteres').isLength({ min: 6 }),
+    validarCampos
+  ],
+  loginUsuario);
 
 router.get('/renew', revalidarToken);
 
