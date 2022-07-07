c73f966ef0ded21193260ec0fe5699d8d393cda3
Enuel <enuel.monzon@gmail.com>
2022-07-07 15:54:30 -0300

    Login - Validación de contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,17 +42,47 @@ const crearUsuario = async(req, res = response ) => {
 
 }
 
-const loginUsuario = (req, res = response ) => {
+const loginUsuario = async(req, res = response ) => {
 
   const { email, password } = req.body;
 
+  try {
 
-  res.json({
-    ok: true,
-    msg: 'Login',
-    email,
-    password
-  });
+    const usuario = await Usuario.findOne({ email });
+
+    if ( !usuario ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El usuario no existe con ese email'
+      });
+    }
+
+    // Confirmar los passwords
+    const validPassword = bcrypt.compareSync( password, usuario.password );
+
+    if ( !validPassword ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'La contraseña no es válida'
+      });
+    }
+
+    // TODO: Generar JWT
+
+    res.json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name
+    })
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador'
+    });
+  }
+
 }
 
 const revalidarToken = (req, res = response ) => {
