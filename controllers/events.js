846cd6121082ad188dd4c7b650f3c7ffbbebee26
Enuel <enuel.monzon@gmail.com>
2022-07-15 22:04:31 -0300

    grabando evento en la base de datos

--- controllers/events.js
+++ controllers/events.js
@@ -1,5 +1,5 @@
-
 const { response } = require('express');
+const Evento = require('../models/Evento');
 
 
 const getEventos = async(req, res = response ) => {
@@ -13,15 +13,26 @@ const getEventos = async(req, res = response ) => {
 
 const crearEvento = async(req, res = response ) => {
 
-  // verificar que tenga el evento.
-  console.log( req.body );
+  const evento = new Evento( req.body );
 
-  res.json({
-    ok: true,
-    msg: 'crearEvento'
-  });
+  try {
 
+    evento.user = req.uid;
 
+    const eventoGuardado = await evento.save();
+
+    res.json({
+      ok: true,
+      evento: eventoGuardado
+    })
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    });
+  }
 
 }
 
--- models/Usuario.js
+++ models/Usuario.js
@@ -3,17 +3,18 @@ const { Schema, model } = require('mongoose');
 const UsuariosSchema = Schema({
   name: {
     type: String,
-    require: true
+    required: true
   },
   email: {
     type: String,
-    require: true,
+    required: true,
     unique: true
   },
   password: {
     type: String,
-    require: true
+    required: true
   }
 });
+
 
 module.exports = model('Usuario', UsuariosSchema );
