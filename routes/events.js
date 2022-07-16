db1ec73a1b1ff74b6e988784a9ab438642d53488
Enuel <enuel.monzon@gmail.com>
2022-07-16 01:25:52 -0300

    validaciones en actualizar evento

--- routes/events.js
+++ routes/events.js
@@ -33,7 +33,15 @@ router.post(
   crearEvento );
 
 // Actualizar evento
-router.put('/:id', actualizarEvento );
+router.put(
+  '/:id',
+  [
+    check('title', 'El título es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+    check('end', 'Fecha de finalización es obligatoria').custom( isDate ),
+    validarCampos
+  ],
+  actualizarEvento );
 
 // Borrar evento
 router.delete('/:id', eliminarEvento );
